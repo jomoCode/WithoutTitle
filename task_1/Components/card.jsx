ee07d8c99bff64295c1d46749feb4e3be5d53771
jomoartz <johnjohananmogbolu@gmail.com>
2025-11-19 22:03:06 +0100

    reorganize with hover

--- task_1/Components/card.jsx
+++ task_1/Components/card.jsx
@@ -2,7 +2,6 @@
 import Image from "next/image";
 import {
   SquarePen,
-  ChevronRight,
   Tag,
   AlertCircleIcon,
   Banknote,
@@ -12,106 +11,119 @@ import {
 } from "lucide-react";
 
 const Card = ({
-  hashTags,
+  hashTags = ["streamer", "gaming"],
   profile_name = "Profile name",
   user_title = "MTA Owner",
   description = ["all about you"],
   status = "Not done",
   profile_image = "https://cdn.pixabay.com/photo/2016/05/05/18/03/coupe-1374448_1280.jpg",
+  header_bg = "https://img.freepik.com/premium-photo/soft-white-curtains-background-graphic-design-wallpaper_226189-17375.jpg",
+  title = "Need a streamer with benefits",
+  budget_status = "Agreed",
+  acceptor_status = "No acceptor found",
+  time_ago = "2 months ago",
+  onView = () => console.log("clicked view"),
 }) => {
   return (
-    <div className="bg-gray-900 border-2 border-gray-600 w-72 h-[560px] rounded-lg overflow-hidden">
+    <div className="bg-gray-900 border-2 border-gray-600 w-72 h-[570px] rounded-lg overflow-hidden transition-all duration-300 hover:border-amber-400 hover:shadow-lg hover:shadow-amber-400/20 hover:-translate-y-1">
       {/* Card header */}
-      {/* profile name */}
       <div
-        className="flex justify-center items-end h-[20%] w-full bg-amber-50 bg-cover bg-center"
+        className="flex justify-center items-end h-[20%] w-full bg-amber-50 bg-cover bg-center transition-all duration-300 hover:h-[22%]"
         style={{
-          backgroundImage:
-            "url('https://img.freepik.com/premium-photo/soft-white-curtains-background-graphic-design-wallpaper_226189-17375.jpg')",
+          backgroundImage: `url('${header_bg}')`,
         }}
       >
-        <div className="py-1 px-2 bg-gray-900 italic font-bold rounded-md hover:bg-gray-700 text-white text-xs flex justify-center items-center flex-row mb-2">
-          <SquarePen size={10} className="text-white/50 mr-2" />
+        <div className="py-1 px-2 bg-gray-900 italic font-bold rounded-md hover:bg-amber-500 hover:text-gray-900 transition-all duration-300 text-white text-xs flex justify-center items-center flex-row mb-2 cursor-pointer hover:scale-105">
+          <SquarePen size={10} className="text-white/50 mr-2 group-hover:text-gray-900" />
           {profile_name}
         </div>
       </div>
-      {/* Card  profile photo &  profile title  */}
-      <div className="flex items-center absolute top-20 p-2 rounded-md">
-        {/* Profile image */}
+
+      {/* Card profile photo & profile title */}
+      <div className="flex items-center absolute top-20 p-2 rounded-md group">
         <Image
           src={profile_image}
           width={30}
           height={30}
           alt="profile photo"
-          className="w-12 h-12 rounded-full object-cover bg-amber-700"
+          className="w-12 h-12 rounded-full object-cover bg-amber-700 transition-all duration-300 hover:scale-110 hover:ring-2 hover:ring-amber-400 cursor-pointer"
         />
 
-        {/* Text + icon */}
-        <span className="absolute top-8 left-16 flex items-center gap-1 text-white/50 text-sm font-medium whitespace-nowrap">
+        <span className="absolute top-8 left-16 flex items-center gap-1 text-white/50 text-sm font-medium whitespace-nowrap transition-colors duration-300 hover:text-amber-400">
           <span className="text-xl">»</span>
           <span>{user_title}</span>
         </span>
       </div>
-      {/*keeps elements from sliding under absolute elements */}
+
       <div className="h-6" />
+
       <main className="p-2">
-        <div className="flex items-center justify-end gap-1 text-sm font-medium whitespace-nowrap mb-4">
-          <h1 className="text-right font-bold">
-            Need a streamer with benefits
+        <div className="flex items-center justify-end gap-1 text-sm font-medium whitespace-nowrap mb-4 group cursor-pointer">
+          <h1 className="text-right font-bold transition-colors duration-300 group-hover:text-amber-400">
+            {title}
           </h1>
-          <Tag size={15} color="blue" />
+          <Tag size={15} color="blue" className="transition-transform duration-300 group-hover:rotate-12" />
         </div>
-        <div className="text-right p-2 bg-gray-950/25 text-xs space-y-1">
-          {description.map((content) => (
-            <font>{content}</font>
+
+        <div className="text-right p-2 bg-gray-950/25 text-xs space-y-1 rounded-md transition-all duration-300 hover:bg-gray-950/40 hover:shadow-md">
+          {description.map((content, index) => (
+            <div key={index}>{content}</div>
           ))}
 
-          {/* Status */}
-          <span className="flex items-center  justify-end gap-1 text-white/50 text-xs font-medium whitespace-nowrap mt-5">
+          <span className="flex items-center justify-end gap-1 text-white/50 text-xs font-medium whitespace-nowrap mt-5 transition-colors duration-300 hover:text-amber-400">
             {status}
             <AlertCircleIcon size={10} />
           </span>
         </div>
-        {/* Budget and agreed  */}
+
         <div className="w-full flex justify-end items-center text-sm mt-3">
-          <div className="px-2 py-1 bg-black rounded-md">Agreed</div>
-          <div className="px-2 py-1 flex justify-end items-center gap-1">
-            Budget <Banknote size={20} />
+          <div className="px-2 py-1 bg-black rounded-md transition-all duration-300 hover:bg-amber-500 hover:text-gray-900 hover:scale-105 cursor-pointer">
+            {budget_status}
+          </div>
+          <div className="px-2 py-1 flex justify-end items-center gap-1 transition-colors duration-300 hover:text-amber-400 cursor-pointer">
+            Budget <Banknote size={20} className="transition-transform duration-300 hover:rotate-12" />
           </div>
         </div>
 
-        {/* Categories */}
-        <span className="p-2 flex justify-end items-center gap-1 text-sm">
+        <span className="p-2 flex justify-end items-center gap-1 text-sm transition-colors duration-300 hover:text-amber-400 cursor-pointer">
           Categories
-          <TagsIcon size={20} />
+          <TagsIcon size={20} className="transition-transform duration-300 hover:rotate-12" />
         </span>
 
-        {/* Hashtags */}
         <div className="flex flex-row text-sm justify-end items-center w-full gap-1">
           {hashTags.map((tag, index) => (
             <div
-              key={`${tag[1]}_${index}`}
-              className="py-1 px-2 bg-gray-800 text-white rounded-xl"
+              key={`${tag}_${index}`}
+              className="py-1 px-2 bg-gray-800 text-white rounded-xl transition-all duration-300 hover:bg-amber-500 hover:text-gray-900 hover:scale-110 cursor-pointer"
             >
               #{tag}
             </div>
           ))}
         </div>
+
         <div className="w-full flex justify-between items-center text-xs mt-4">
-          <div className="text-amber-400">No acceptor found</div>
-          <div className="flex justify-end items-center gap-1">
-            2 months ago <Calendar size={10} />
+          <div className="text-amber-400 transition-all duration-300 hover:text-amber-300 hover:scale-105">
+            {acceptor_status}
+          </div>
+          <div className="flex justify-end items-center gap-1 transition-colors duration-300 hover:text-amber-400 cursor-pointer">
+            {time_ago} <Calendar size={10} />
           </div>
         </div>
       </main>
+
       {/* Footer */}
       <div className="border-t-amber-200 border-t">
         <div
-          className="text-lg bg-linear-to-b from-amber-300/40  via-amber-300/15 to-amber-300/5"
-          onClick={() => console.log("clicked view")}
+          className="text-lg bg-linear-to-b from-amber-300/40 via-amber-300/15 to-amber-300/5 transition-all duration-300 hover:from-amber-400/60 hover:via-amber-400/30 hover:to-amber-400/10 cursor-pointer"
+          onClick={onView}
         >
-          <div className="text-sm flex justify-center items-center gap-1 py-2">
-            View <ExternalLink color="yellow" size={15} />
+          <div className="text-sm flex justify-center items-center gap-1 py-2 group">
+            <span className="transition-transform duration-300 group-hover:-translate-x-[4]">View</span>
+            <ExternalLink 
+              color="yellow" 
+              size={15} 
+              className="transition-transform duration-300 group-hover:translate-x-1 group-hover:-translate-y-px"
+            />
           </div>
         </div>
       </div>
@@ -119,4 +131,4 @@ const Card = ({
   );
 };
 
-export { Card };
+export {Card};
