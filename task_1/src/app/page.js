c50e49603137ae88f06abb3861ee51d6a8ef16b5
jomoartz <johnjohananmogbolu@gmail.com>
2025-11-19 19:51:56 +0100

    recreated app as js

--- task_1/src/app/page.js
+++ task_1/src/app/page.js
@@ -1,10 +1,9 @@
-
 import { Card } from "../Components/card";
 
 export default function Home() {
   return (
     <main className="flex flex-1 justify-center items-center">
-      <Card hashTags={['lalala', 'lalala', 'ausxxhhxh']}/>
+      <Card hashTags={["lalala", "lalala", "ausxxhhxh"]} />
     </main>
   );
 }
